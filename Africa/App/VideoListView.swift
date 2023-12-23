2e9a16bca59517865a579cb0a30a631f8919d1c0
sergiyvergun <sergiyvergun01@gmail.com>
2023-12-23 17:49:55 +0100

    Implement video list

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct VideoListView: View {
+    
+    @State var videos : [Video] = Bundle.main.decode("videos.json")
+    
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical,8)
+                }
+            }.listStyle(InsetGroupedListStyle())
+                .navigationBarTitle("Videos",displayMode: .inline)
+                .toolbar{
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        Button(action: {
+                            videos.shuffle()
+                            hapticImpact.impactOccurred()
+                        }){
+                            Image(systemName: "arrow.2.squarepath")
+                        }
+                    }
+                }
+        }
     }
 }
 
--- Africa/Model/VideoModel.swift
+++ Africa/Model/VideoModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Video: Codable, Identifiable{
+    let id: String
+    let name: String
+    let headline: String
+    
+    //Computed property
+    var thumbnail: String{
+        "video-\(id)"
+    }
+}
