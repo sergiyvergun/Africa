7d143271c47e5dc76b70047c2723995eb8f22619
sergiyvergun <sergiyvergun01@gmail.com>
2024-01-16 19:24:54 +0100

    Add ui tests

--- AfricaUITests/AfricaUITests.swift
+++ AfricaUITests/AfricaUITests.swift
@@ -10,24 +10,40 @@ import XCTest
 final class AfricaUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+            
+        // Check if bottom bar tab buttons exist
+        let watchTabButton = XCUIApplication().tabBars["Tab Bar"].buttons["Watch"]
+        XCTAssertTrue(watchTabButton.exists)
+        let browseTabButton = XCUIApplication().tabBars["Tab Bar"].buttons["Browse"]
+        XCTAssertTrue(browseTabButton.exists)
+        let mapTabButton = XCUIApplication().tabBars["Tab Bar"].buttons["Map"]
+        XCTAssertTrue(mapTabButton.exists)
+        let galleryTabButton = XCUIApplication().tabBars["Tab Bar"].buttons["Gallery"]
+        XCTAssertTrue(galleryTabButton.exists)
+        
+        
+        //Check if tab buttons working correctly
+        browseTabButton.tap()
+        XCTAssertTrue(app.navigationBars["Africa"].exists)
+        
+        watchTabButton.tap()
+        XCTAssertTrue(app.navigationBars["Videos"].exists)
+        
+        mapTabButton.tap()
+        XCTAssertTrue(app.staticTexts["Latitude:"].exists)
+        XCTAssertTrue(app.staticTexts["Longitude:"].exists)
+        
+        
+        
     }
 
     func testLaunchPerformance() throws {
